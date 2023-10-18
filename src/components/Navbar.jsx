e5bfb564c11c845215bbec093c37cb46ac50d4c5
Raiyan109 <kraiyan109@gmail.com>
2023-10-18 22:35:20 +0600

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import { BiSearch } from 'react-icons/bi'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { BsFillPersonFill } from 'react-icons/bs'
 import { Link } from 'react-router-dom'
@@ -11,15 +10,9 @@ const Navbar = () => {
                         {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                         </svg> */}
-                        <span className="ml-3 text-xl">AirBnb</span>
+                        <span className="ml-3 text-xl">TechNext</span>
                     </a>
-                    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-                        <a className="mr-5 hover:text-gray-900">Any where</a>
-                        <a className="mr-5 hover:text-gray-900">Any week</a>
-                        <a className="mr-5 hover:text-gray-900">Add guest</a>
-                        <div><BiSearch /></div>
 
-                    </nav>
                     <div className='flex justify-center items-center gap-2 border-gray-200 shadow-md p-3 rounded-xl'>
                         <div><GiHamburgerMenu /></div>
                         <Link to='/login'><BsFillPersonFill /></Link>
